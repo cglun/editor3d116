505c8dc70823f659ea1bd29a04048646878d5219
lilun <oklun@qq.com>
2025-01-23 00:09:32 +0800

    license

--- src/routes/about.lazy.tsx
+++ src/routes/about.lazy.tsx
@@ -1,12 +1,12 @@
-import { createLazyFileRoute } from '@tanstack/react-router';
+import { createLazyFileRoute } from "@tanstack/react-router";
 
-import { Button, ListGroup, ListGroupItem } from 'react-bootstrap';
-import { setClassName } from '../app/utils';
-import { getThemeColor } from '../app/config';
-import AlertBase from '../component/AlertBase';
-import { APP_COLOR } from '../type';
+import { Button, ListGroup, ListGroupItem } from "react-bootstrap";
+import { setClassName } from "../app/utils";
+import { getThemeColor } from "../app/config";
+import AlertBase from "../component/AlertBase";
+import { APP_COLOR } from "../type";
 
-export const Route = createLazyFileRoute('/about')({
+export const Route = createLazyFileRoute("/about")({
   component: RouteComponent,
 });
 
@@ -23,14 +23,14 @@ function RouteComponent() {
     // </Button>
     <ListGroup>
       <ListGroupItem>
-        <a href="https://github.com/cglun/3d-116" target="_blank">
+        <a href="https://github.com/cglun/editor3d116" target="_blank">
           <Button variant={getThemeColor()}>
             <i className="bi bi-github"></i> 源代码
           </Button>
         </a>
         <a href="https://3d.oklun.com" className="ms-2" target="_blank">
           <Button variant={getThemeColor()}>
-            <i className={setClassName('eye')}></i> 预览APP
+            <i className={setClassName("eye")}></i> 预览APP
           </Button>
         </a>
       </ListGroupItem>
@@ -39,7 +39,7 @@ function RouteComponent() {
           <AlertBase
             type={APP_COLOR.Success}
             text={
-              '【名称：3d116】【版本：1.1.6】【制作：李论】【先定个小目标，活个116岁】'
+              "【名称：3d116】【版本：1.1.6】【制作：李论】【先定个小目标，活个116岁】"
             }
           ></AlertBase>
         </div>
