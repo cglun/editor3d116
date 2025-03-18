e38019dca9033a06f9be433a0cf77f9efcc0a07d
lilun <oklun@qq.com>
2025-03-18 21:48:46 +0800

    f

--- src/routes/editor3d/effects.lazy.tsx
+++ src/routes/editor3d/effects.lazy.tsx
@@ -1,9 +1,9 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { createLazyFileRoute } from "@tanstack/react-router";
 
-export const Route = createLazyFileRoute('/editor3d/effects')({
+export const Route = createLazyFileRoute("/editor3d/effects")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/editor3d/efects"!</div>
+  return <div>Hello "/editor3d/efects"116</div>;
 }
